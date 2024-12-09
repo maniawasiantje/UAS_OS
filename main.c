d9f75425f4a1fa110d50d6ab7603c2423176cd49
maniawasiantje <nom065080@gmail.com>
2024-12-08 21:43:15 -0500

    masih ada kendala

---
+++ main.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include "ance.h"
+
+int main()
+{
+
+    printf ("\t\t Library sistem operasi");
+    printf ("\n =============================== \n");
+    printf ("Welcome to converter money");
+    printf ("\n =============================== \n");
+    printf ("\n nilai dolar ke rupiah \n");
+    rupiah ();
+
+
+    printf ("\n nilai rupiah ke dolar \n");
+    dolar ();
+
+}
